96f58250602370e08ff103e2abed9e8d4a22c875
194pp <194pp@users.noreply.github.com>
2020-04-09 18:10:36 +0300

    commit test

--- AttendanceTrackerApp/attendance.Designer.cs
+++ AttendanceTrackerApp/attendance.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AttendanceTrackerApp
 {
-    partial class Form1
+    partial class attendance
     {
         /// <summary>
         /// Required designer variable.
@@ -40,16 +40,19 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.registracijaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notificationTimer = new System.Windows.Forms.Timer(this.components);
+            this.notificationLabel = new System.Windows.Forms.Label();
+            this.pavadinimas = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // sysTime
             // 
             this.sysTime.AutoSize = true;
-            this.sysTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sysTime.Location = new System.Drawing.Point(471, 47);
+            this.sysTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sysTime.Location = new System.Drawing.Point(9, 357);
             this.sysTime.Name = "sysTime";
-            this.sysTime.Size = new System.Drawing.Size(101, 31);
+            this.sysTime.Size = new System.Drawing.Size(59, 20);
             this.sysTime.TabIndex = 0;
             this.sysTime.Text = "Laikas:";
             this.sysTime.Click += new System.EventHandler(this.sysTime_Click);
@@ -64,29 +67,30 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(78, 105);
+            this.label1.Location = new System.Drawing.Point(9, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(108, 16);
-            this.label1.TabIndex = 1;
+            this.label1.TabIndex = 5;
             this.label1.Text = "Darbuotojo ID:";
             // 
             // userEntryID
             // 
+            this.userEntryID.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.userEntryID.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userEntryID.Location = new System.Drawing.Point(81, 124);
+            this.userEntryID.Location = new System.Drawing.Point(12, 96);
             this.userEntryID.Name = "userEntryID";
             this.userEntryID.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.userEntryID.Size = new System.Drawing.Size(257, 29);
-            this.userEntryID.TabIndex = 2;
-            this.userEntryID.UseWaitCursor = true;
+            this.userEntryID.TabIndex = 1;
             // 
             // pradetiDarbaButton
             // 
             this.pradetiDarbaButton.BackColor = System.Drawing.Color.LawnGreen;
+            this.pradetiDarbaButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pradetiDarbaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pradetiDarbaButton.Location = new System.Drawing.Point(506, 342);
+            this.pradetiDarbaButton.Location = new System.Drawing.Point(12, 254);
             this.pradetiDarbaButton.Name = "pradetiDarbaButton";
-            this.pradetiDarbaButton.Size = new System.Drawing.Size(100, 100);
+            this.pradetiDarbaButton.Size = new System.Drawing.Size(130, 100);
             this.pradetiDarbaButton.TabIndex = 3;
             this.pradetiDarbaButton.Text = "Pradėti darbą";
             this.pradetiDarbaButton.UseVisualStyleBackColor = false;
@@ -95,10 +99,11 @@
             // baigtiDarbaButton
             // 
             this.baigtiDarbaButton.BackColor = System.Drawing.Color.Red;
+            this.baigtiDarbaButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.baigtiDarbaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.baigtiDarbaButton.Location = new System.Drawing.Point(612, 342);
+            this.baigtiDarbaButton.Location = new System.Drawing.Point(139, 254);
             this.baigtiDarbaButton.Name = "baigtiDarbaButton";
-            this.baigtiDarbaButton.Size = new System.Drawing.Size(100, 100);
+            this.baigtiDarbaButton.Size = new System.Drawing.Size(130, 100);
             this.baigtiDarbaButton.TabIndex = 4;
             this.baigtiDarbaButton.Text = "Baigti darbą";
             this.baigtiDarbaButton.UseVisualStyleBackColor = false;
@@ -106,19 +111,20 @@
             // 
             // userEntryPass
             // 
+            this.userEntryPass.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.userEntryPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userEntryPass.Location = new System.Drawing.Point(81, 178);
+            this.userEntryPass.Location = new System.Drawing.Point(12, 150);
             this.userEntryPass.Name = "userEntryPass";
+            this.userEntryPass.PasswordChar = '*';
             this.userEntryPass.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.userEntryPass.Size = new System.Drawing.Size(257, 29);
-            this.userEntryPass.TabIndex = 5;
-            this.userEntryPass.UseWaitCursor = true;
+            this.userEntryPass.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(78, 159);
+            this.label2.Location = new System.Drawing.Point(9, 131);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(94, 16);
             this.label2.TabIndex = 6;
@@ -130,7 +136,7 @@
             this.toolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
             this.menuStrip1.TabIndex = 7;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -145,16 +151,45 @@
             // registracijaToolStripMenuItem
             // 
             this.registracijaToolStripMenuItem.Name = "registracijaToolStripMenuItem";
-            this.registracijaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.registracijaToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
             this.registracijaToolStripMenuItem.Text = "Registracija";
             this.registracijaToolStripMenuItem.Click += new System.EventHandler(this.registracijaToolStripMenuItem_Click);
             // 
-            // Form1
+            // notificationTimer
+            // 
+            this.notificationTimer.Interval = 5000;
+            this.notificationTimer.Tick += new System.EventHandler(this.notificationTimer_Tick);
+            // 
+            // notificationLabel
+            // 
+            this.notificationLabel.AutoSize = true;
+            this.notificationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.notificationLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.notificationLabel.Location = new System.Drawing.Point(12, 196);
+            this.notificationLabel.Name = "notificationLabel";
+            this.notificationLabel.Size = new System.Drawing.Size(131, 18);
+            this.notificationLabel.TabIndex = 8;
+            this.notificationLabel.Text = "notificationLabel";
+            this.notificationLabel.TextChanged += new System.EventHandler(this.notificationLabel_TextChange);
+            // 
+            // pavadinimas
+            // 
+            this.pavadinimas.AutoSize = true;
+            this.pavadinimas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pavadinimas.Location = new System.Drawing.Point(7, 37);
+            this.pavadinimas.Name = "pavadinimas";
+            this.pavadinimas.Size = new System.Drawing.Size(263, 24);
+            this.pavadinimas.TabIndex = 9;
+            this.pavadinimas.Text = "Darbo laiko sekimo sistema";
+            // 
+            // attendance
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Ivory;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(284, 385);
+            this.Controls.Add(this.pavadinimas);
+            this.Controls.Add(this.notificationLabel);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.userEntryPass);
             this.Controls.Add(this.baigtiDarbaButton);
@@ -163,7 +198,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.sysTime);
             this.Controls.Add(this.menuStrip1);
-            this.Name = "Form1";
+            this.Name = "attendance";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -185,6 +220,9 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem registracijaToolStripMenuItem;
+        private System.Windows.Forms.Timer notificationTimer;
+        private System.Windows.Forms.Label notificationLabel;
+        private System.Windows.Forms.Label pavadinimas;
     }
 }
 
---
+++ AttendanceTrackerApp/attendance.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AttendanceTrackerApp
+{
+    public partial class attendance : Form
+    {
+        static List<string> logins = new List<string>();
+        static List<string> passwords = new List<string>();
+        static List<string> qr = new List<string>();
+        static List<bool> clockedIn = new List<bool>();
+
+        public attendance()
+        {
+            logins.Add("user1");
+            passwords.Add("password1");
+            clockedIn.Add(false);
+            logins.Add("user2");
+            passwords.Add("password2");
+            clockedIn.Add(false);
+            logins.Add("user3");
+            passwords.Add("password3");
+            clockedIn.Add(false);           
+            InitializeComponent();
+            notificationLabel.Text = "";
+
+        }
+
+        private void sysTime_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sysTime.Text = "Laikas: " + DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void registracijaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var regPopup = new Form();
+            regPopup.Show(this);
+        }
+
+        private void clearInput()
+        {
+            userEntryID.Text = "";
+            userEntryPass.Text = "";
+        }
+
+        private void pradetiDarbaButton_Click(object sender, EventArgs e)
+        {
+            if (logins.Contains(userEntryID.Text))
+            {
+                int index = logins.IndexOf(userEntryID.Text);
+                if (userEntryPass.Text == passwords[index])
+                {
+                    if (!clockedIn[index])
+                    {
+                        clockedIn[index] = true;
+                        notificationLabel.ForeColor = System.Drawing.Color.Green;
+                        notificationLabel.Text = "Pradėjote darbą:  " + logins[index] + "\nLaikas: "  + DateTime.Now.ToString("HH:mm:ss");                
+                        clearInput();
+                    }
+                    else
+                    {
+                        notificationLabel.Text = "Jau esate prisijungę!";
+                    }
+                }
+                else
+                {
+                    notificationLabel.Text = "Wrong Password!";
+                    userEntryPass.Text = "";
+                }
+            }
+            else
+            {
+                notificationLabel.Text = "There's no such user!";
+                clearInput();
+            }
+        }
+       
+        private void baigtiDarbaButton_Click(object sender, EventArgs e)
+        {
+            if (logins.Contains(userEntryID.Text))
+            {
+                int index = logins.IndexOf(userEntryID.Text);
+                if (userEntryPass.Text == passwords[index])
+                {
+                    if (clockedIn[index])
+                    {
+                        clockedIn[index] = false;
+                        notificationLabel.ForeColor = System.Drawing.Color.Red;
+                        notificationLabel.Text = "Baigėte darbą: " + logins[index] + "\nLaikas: " + DateTime.Now.ToString("HH:mm:ss");
+                        clearInput();
+                    }
+                    else
+                    {
+                        notificationLabel.Text = "Jau nesate prisijungę!";
+                    }
+                }
+                else
+                {
+                    notificationLabel.Text = "Wrong Password!";
+                    userEntryPass.Text = "";
+                }
+            }
+            else
+            {
+                notificationLabel.Text = "There's no such user!";
+                clearInput();
+            }
+        }
+
+        private void notificationLabel_TextChange(object sender, EventArgs e)
+        {
+            if (notificationLabel.Text != "")
+            {
+                notificationTimer.Enabled = false;
+                notificationTimer.Enabled = true;
+            }
+        }
+
+        private void notificationTimer_Tick(object sender, EventArgs e)
+        {
+            notificationLabel.Text = "";
+            notificationLabel.ForeColor = System.Drawing.Color.Black;
+            notificationTimer.Enabled = false;
+        }
+    }
+}
+
+///
